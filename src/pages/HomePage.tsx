562b40acd995c177b15fd5446309e363c1c1d83c
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2019-12-22 13:43:43 +0100

    homepage: buttons

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,7 +2,12 @@ import React from 'react'
 import { useAsync } from 'react-use'
 import context from '../context'
 import { getItems } from '../providers/item'
-import { Paper, Table, TableHead, TableRow, TableCell, TableBody } from '@material-ui/core'
+import {
+  Add as AddIcon,
+  Edit as EditIcon,
+  Delete as DeleteIcon
+} from '@material-ui/icons'
+import { Paper, Table, TableHead, TableRow, TableCell, TableBody, IconButton } from '@material-ui/core'
 
 export default () => {
   const { token } = React.useContext(context)
@@ -16,7 +21,12 @@ export default () => {
         <TableHead>
           <TableRow>
             <TableCell>Title</TableCell>
-            <TableCell align="right">Description</TableCell>
+            <TableCell>Description</TableCell>
+            <TableCell align="right">
+              <IconButton title="Add new item">
+                <AddIcon />
+              </IconButton>
+            </TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
@@ -25,7 +35,15 @@ export default () => {
               <TableCell component="th" scope="row">
                 {item.title}
               </TableCell>
-              <TableCell align="right">{item.description}</TableCell>
+              <TableCell>{item.description}</TableCell>
+              <TableCell align="right" title="Edit item">
+                <IconButton>
+                  <EditIcon />
+                </IconButton>
+                <IconButton color="secondary" title="Delete item">
+                  <DeleteIcon />
+                </IconButton>
+              </TableCell>
             </TableRow>
           ))}
         </TableBody>
