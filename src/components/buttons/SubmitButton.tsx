2e533049b3a510e0da96e693c3058dd6c5392ea2
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2019-12-24 23:33:50 +0100

    recode

--- src/components/buttons/SubmitButton.tsx
+++ src/components/buttons/SubmitButton.tsx
@@ -2,11 +2,9 @@ import React from 'react'
 import { KeyboardArrowRight as KeyboardArrowRightIcon, HourglassEmpty as HourglassEmptyIcon } from '@material-ui/icons'
 import { Button, ButtonProps } from '@material-ui/core'
 
-export default ({ type = 'submit', disabled, children, ...rest }: ButtonProps) => {
-  return (
-    <Button type={type} disabled={disabled} {...rest}>
-      {children}
-      {disabled ? <HourglassEmptyIcon /> : <KeyboardArrowRightIcon />}
-    </Button>
-  )
-}
+export default ({ type = 'submit', disabled, children, ...rest }: ButtonProps) => (
+  <Button type={type} disabled={disabled} {...rest}>
+    {children}
+    {disabled ? <HourglassEmptyIcon /> : <KeyboardArrowRightIcon />}
+  </Button>
+)
