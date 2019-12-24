8456712b72eb09c8e70a2ebffe95d06a543937fd
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2019-12-24 20:58:59 +0100

    recode: single import

--- src/routes/LoginDialog.tsx
+++ src/routes/LoginDialog.tsx
@@ -1,13 +1,7 @@
 import React from 'react'
 import { useAsyncFn } from 'react-use'
 import { useTranslation } from 'react-i18next'
-import Button from '@material-ui/core/Button'
-import Dialog from '@material-ui/core/Dialog'
-import DialogActions from '@material-ui/core/DialogActions'
-import DialogContent from '@material-ui/core/DialogContent'
-import DialogTitle from '@material-ui/core/DialogTitle'
-import TextField from '@material-ui/core/TextField'
-import Typography from '@material-ui/core/Typography'
+import { Button, Dialog, DialogTitle, DialogContent, DialogActions, TextField, Typography } from '@material-ui/core'
 import { appContext } from '../context'
 
 export default () => {
