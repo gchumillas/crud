3e6e038078b91acd0109660a241ca11723270fe2
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2019-12-24 23:41:42 +0100

    routes: not found dialog

--- src/routes/NotFoundDialog.tsx
+++ src/routes/NotFoundDialog.tsx
@@ -1,6 +1,24 @@
 import React from 'react'
+import { useTranslation } from 'react-i18next'
+import { useHistory } from 'react-router-dom'
+import { Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from '@material-ui/core'
+import SubmitButton from '../components/buttons/SubmitButton'
 
-// TODO: make this page
 export default () => {
-  return <div>Page not found</div>
+  const history = useHistory()
+  const { t } = useTranslation()
+
+  return (
+    <Dialog open fullWidth maxWidth="xs">
+      <DialogTitle>{t('routes.notFound.title')}</DialogTitle>
+      <DialogContent>
+        <DialogContentText>
+          {t('routes.notFound.text')}
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <SubmitButton onClick={() => history.push('/')}>{t('buttons.continue')}</SubmitButton>
+      </DialogActions>
+    </Dialog>
+  )
 }
