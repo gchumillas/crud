5308bc8cd1a4c65e0c429a20b901adeede32de30
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2019-12-24 21:29:07 +0100

    component: ProtectedRoute

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-// TODO: (package.json) some dependencies should be better under devDependencies (for example: @typescript, @types/react, etc...)
-// TODO: (package.json) disable automatic browse opening
 // TODO: use absolute paths
 import React from 'react'
-import { BrowserRouter, Route, Switch, Redirect, RouteProps } from 'react-router-dom'
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
 import { CssBaseline, Container } from '@material-ui/core'
 import { appContext } from './context'
 import { login as loginProvider } from './providers/user'
 import Header from './components/Header'
+import ProtectedRoute from './components/ProtectedRoute'
 import LoginDialog from './routes/LoginDialog'
 import HomePage from './routes/HomePage'
 import './i18n'
@@ -27,24 +26,6 @@ const App = () => {
     setToken('')
   }
 
-  // TODO: should be in a separate file
-  const ProtectedRoute = ({ component: Component, ...rest }: RouteProps) => {
-    if (!Component) return null
-
-    return (
-      <Route
-        {...rest}
-        render={props => {
-          const referrer = props.location.pathname
-
-          return token
-            ? <Component {...props} />
-            : <Redirect to={{ pathname: '/login', state: { referrer } }} />
-        }}
-      />
-    )
-  }
-
   return (
     <appContext.Provider value={{ token, login, logout }}>
       <CssBaseline />
@@ -62,7 +43,7 @@ const App = () => {
                 return token ? <Redirect to={page} /> : <LoginDialog />
               }}
             />
-            <ProtectedRoute path="/" component={HomePage} />
+            <ProtectedRoute token={token} path="/" component={HomePage} />
           </Switch>
         </BrowserRouter>
       </Container>
