d97216426e74a4b1146d380a89e3e730c4b73efd
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2019-12-25 12:53:44 +0100

    http errors

--- src/lib/http.ts
+++ src/lib/http.ts
@@ -1,5 +1,10 @@
+import _ from 'lodash'
 import axios from 'axios'
+import { TFunction } from 'i18next'
 import { API_URL } from './env'
+
+export const HTTP_FORBIDDEN = 403
+export const HTTP_UNKNOWN = 666
 
 export const http = (token: string) => {
   return axios.create({
@@ -8,4 +13,14 @@ export const http = (token: string) => {
       ...(token && { Authorization: `Bearer ${token}` })
     }
   })
+}
+
+export const getStatus = (error: Error): number => {
+  const response = _.get(error, 'response')
+
+  return _.get(response, 'status', HTTP_UNKNOWN)
+}
+
+export const translateStatus = (t: TFunction, path: string, errorCode: number) => {
+  return t(`${path}.http.${errorCode}`, t(`http.${errorCode}`, t(`http.${HTTP_UNKNOWN}`)))
 }
--- src/routes/LoginDialog.tsx
+++ src/routes/LoginDialog.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import { useAsyncFn } from 'react-use'
 import { useTranslation } from 'react-i18next'
-import { DialogTitle, DialogContent, DialogActions, Typography } from '@material-ui/core'
+import { DialogTitle, DialogContent, DialogActions, DialogContentText } from '@material-ui/core'
 import { appContext } from '../lib/context'
+import { getStatus, translateStatus } from '../lib/http'
 import SubmitButton from '../components/buttons/SubmitButton'
 import TextField from '../components/fields/TextField'
 import Dialog from '../components/Dialog'
@@ -13,6 +14,7 @@ export default () => {
   const [username, setUsername] = React.useState('')
   const [password, setPassword] = React.useState('')
   const [state, submit] = useAsyncFn(() => login(username, password), [username, password])
+  const status = state.error && getStatus(state.error)
 
   // TODO: notify http errors
   return (
@@ -24,10 +26,11 @@ export default () => {
         <TextField autoFocus autoComplete="username" label={t('routes.login.usernameField')} value={username} onChange={setUsername} />
         <TextField autoComplete="password" type="password" label={t('routes.login.passwordField')} value={password} onChange={setPassword} />
       </DialogContent>
-      <DialogContent>
-        {/* TODO: show a custom error message */}
-        {state.error && <Typography color="error">{state.error.message}</Typography>}
-      </DialogContent>
+      {status && (
+        <DialogContent>
+          <DialogContentText color="error">{translateStatus(t, 'routes.login', status)}</DialogContentText>
+        </DialogContent>
+      )}
       <DialogActions>
         <SubmitButton disabled={state.loading} onClick={submit}>{t('buttons.continue')}</SubmitButton>
       </DialogActions>
