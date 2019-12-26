f7b20ef9346b5dcbc0097bb3e827aa1e6d36bc45
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2019-12-26 18:28:00 +0100

    components: SelectField

--- src/routes/LoginDialog.tsx
+++ src/routes/LoginDialog.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { useAsyncFn } from 'react-use'
 import { useTranslation } from 'react-i18next'
-import { Dialog, DialogTitle, DialogContent, DialogActions, DialogContentText, Select, FormControl, InputLabel, MenuItem } from '@material-ui/core'
+import { Dialog, DialogTitle, DialogContent, DialogActions, DialogContentText } from '@material-ui/core'
 import { appContext } from '../lib/context'
 import { getErrorStatus } from '../lib/http'
 import { getShortLang } from '../lib/i18n'
-import SubmitButton from '../components/buttons/SubmitButton'
+import SelectField from '../components/fields/SelectField'
 import TextField from '../components/fields/TextField'
+import SubmitButton from '../components/buttons/SubmitButton'
 
 export default () => {
   const { t, i18n } = useTranslation()
@@ -16,6 +17,7 @@ export default () => {
   const [password, setPassword] = React.useState('')
   const [state, submit] = useAsyncFn(() => login(username, password), [username, password])
   const status = state.error && getErrorStatus(state.error)
+  const languages = ['en', 'es'].map(value => ({ value, label: t(`routes.login.${value}`) }))
 
   const onLanguageChange = (value: string) => {
     i18n.changeLanguage(value)
@@ -28,13 +30,7 @@ export default () => {
       <DialogTitle>{t('routes.login.title')}</DialogTitle>
       <DialogContent>
         {/* TODO: add a 'remember username' checkbox */}
-        <FormControl fullWidth>
-          <InputLabel>{t('routes.login.language')}</InputLabel>
-          <Select value={language} onChange={e => onLanguageChange(`${e.target.value}`)}>
-            <MenuItem value="en">{t('routes.login.english')}</MenuItem>
-            <MenuItem value="es">{t('routes.login.spanish')}</MenuItem>
-          </Select>
-        </FormControl>
+        <SelectField label={t('routes.login.language')} value={language} options={languages} onChange={onLanguageChange} />
         <TextField autoFocus autoComplete="username" label={t('routes.login.usernameField')} value={username} onChange={setUsername} />
         <TextField autoComplete="password" type="password" label={t('routes.login.passwordField')} value={password} onChange={setPassword} />
       </DialogContent>
