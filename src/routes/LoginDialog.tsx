b266101ab2811514ff2c0e2902ad9b9f806a5c13
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2019-12-26 19:30:54 +0100

    discard inline to-do

--- src/routes/LoginDialog.tsx
+++ src/routes/LoginDialog.tsx
@@ -28,7 +28,6 @@ export default () => {
     <Dialog open fullWidth maxWidth="xs" onKeyDown={e => e.key === 'Enter' && submit()}>
       <DialogTitle>{t('routes.login.title')}</DialogTitle>
       <DialogContent>
-        {/* TODO: add a 'remember username' checkbox */}
         <SelectField label={t('routes.login.language')} value={language} options={languages} onChange={onLanguageChange} />
         <TextField autoFocus autoComplete="username" label={t('routes.login.usernameField')} value={username} onChange={setUsername} />
         <TextField autoComplete="password" type="password" label={t('routes.login.passwordField')} value={password} onChange={setPassword} />
