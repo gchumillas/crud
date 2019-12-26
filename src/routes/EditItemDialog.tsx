a5abfab84e6a540d7fdba26100dfcc09368ddfd6
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2019-12-26 14:11:39 +0100

    manage server errors

--- src/routes/EditItemDialog.tsx
+++ src/routes/EditItemDialog.tsx
@@ -2,8 +2,9 @@ import React from 'react'
 import { useAsyncFn, useAsync } from 'react-use'
 import { useTranslation } from 'react-i18next'
 import { useHistory, useParams } from 'react-router-dom'
-import { Button, Dialog, DialogTitle, DialogContent, DialogActions, Typography } from '@material-ui/core'
+import { Button, Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions } from '@material-ui/core'
 import { appContext, pageContext } from '../lib/context'
+import { getErrorStatus } from '../lib/http'
 import { readItem, updateItem } from '../providers/item'
 import SubmitButton from '../components/buttons/SubmitButton'
 import TextField from '../components/fields/TextField'
@@ -17,22 +18,20 @@ export default () => {
   const { refresh } = React.useContext(pageContext)
   const [title, setTitle] = React.useState('')
   const [description, setDescription] = React.useState('')
-
   const initState = useAsync(async () => {
     const data = await readItem(token, params.id)
 
     setTitle(data.title)
     setDescription(data.description)
   }, [params.id])
-
   const [submitState, onSubmit] = useAsyncFn(async () => {
     await updateItem(token, params.id, title, description)
     refresh()
     history.push('/')
   }, [params.id, title, description])
-
-  const loading = initState.loading || submitState.loading
-  const error = initState.error || submitState.error
+  const loading = submitState.loading || initState.loading
+  const error = submitState.error || initState.error
+  const status = error && getErrorStatus(error)
 
   return (
     <Dialog open>
@@ -41,9 +40,11 @@ export default () => {
         <TextField autoFocus required label={t('routes.editItem.titleField')} value={title} onChange={setTitle} />
         <TextArea label={t('routes.editItem.descriptionField')} value={description} onChange={setDescription} />
       </DialogContent>
-      <DialogContent>
-        {error && <Typography color="error">{error.message}</Typography>}
-      </DialogContent>
+      {status && (
+        <DialogContent>
+          <DialogContentText color="error">{t(`http.${status}`)}</DialogContentText>
+        </DialogContent>
+      )}
       <DialogActions>
         <Button disabled={loading} onClick={() => history.push('/')}>{t('buttons.cancel')}</Button>
         <SubmitButton disabled={loading} onClick={onSubmit}>{t('buttons.continue')}</SubmitButton>
