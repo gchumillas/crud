dc224b66117358b57f947fb5424182e73b8c909e
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2019-12-26 13:57:59 +0100

    manage server errors

--- src/routes/CreateItemDialog.tsx
+++ src/routes/CreateItemDialog.tsx
@@ -2,8 +2,9 @@ import React from 'react'
 import { useAsyncFn } from 'react-use'
 import { useHistory } from 'react-router-dom'
 import { useTranslation } from 'react-i18next'
-import { Button, Dialog, DialogTitle, DialogContent, DialogActions, Typography } from '@material-ui/core'
+import { Button, Dialog, DialogTitle, DialogContent, DialogActions, DialogContentText } from '@material-ui/core'
 import { appContext, pageContext } from '../lib/context'
+import { getErrorStatus } from '../lib/http'
 import { createItem } from '../providers/item'
 import SubmitButton from '../components/buttons/SubmitButton'
 import TextField from '../components/fields/TextField'
@@ -21,6 +22,7 @@ export default () => {
     refresh()
     history.push('/')
   }, [title, description])
+  const status = state.error && getErrorStatus(state.error)
 
   return (
     <Dialog open>
@@ -29,9 +31,11 @@ export default () => {
         <TextField autoFocus required label={t('routes.createItem.titleField')} value={title} onChange={setTitle} />
         <TextArea label={t('routes.createItem.descriptionField')} value={description} onChange={setDescription} />
       </DialogContent>
-      <DialogContent>
-        {state.error && <Typography color="error">{state.error.message}</Typography>}
-      </DialogContent>
+      {status && (
+        <DialogContent>
+          <DialogContentText color="error">{t(`http.${status}`)}</DialogContentText>
+        </DialogContent>
+      )}
       <DialogActions>
         <Button disabled={state.loading} onClick={() => history.push('/')}>{t('buttons.cancel')}</Button>
         <SubmitButton disabled={state.loading} onClick={onSubmit}>{t('buttons.continue')}</SubmitButton>
