2aaebd8cffa4cdf23501a3465c8f74ce4d94697c
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2020-01-06 18:47:55 +0100

    codefix

--- src/components/fields/SelectField.tsx
+++ src/components/fields/SelectField.tsx
@@ -3,19 +3,17 @@ import { Select, SelectProps, FormControl, InputLabel, MenuItem } from '@materia
 
 type Props = Omit<SelectProps, 'onChange'> & {
   label?: string,
-  options: {label: string, value: string}[],
+  options: { label: string, value: string }[],
   onChange?: (value: string) => void
 }
 
-export default ({ label, value, options, onChange }: Props) => {
-  return (
-    <FormControl fullWidth>
-      {label && <InputLabel>{label}</InputLabel>}
-      <Select value={value} onChange={e => onChange && onChange(`${e.target.value}`)}>
-        {options.map(({ value, label }, key) => (
-          <MenuItem key={key} value={value}>{label}</MenuItem>
-        ))}
-      </Select>
-    </FormControl>
-  )
-}
+export default ({ label, value, options, onChange }: Props) => (
+  <FormControl fullWidth>
+    {label && <InputLabel>{label}</InputLabel>}
+    <Select value={value} onChange={e => onChange && onChange(`${e.target.value}`)}>
+      {options.map(({ value, label }, key) => (
+        <MenuItem key={key} value={value}>{label}</MenuItem>
+      ))}
+    </Select>
+  </FormControl>
+)
